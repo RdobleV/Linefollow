2b6295ec022964180e98ac5c7c063580ac0dfde3
RdobleV <mrx235@gmail.com>
2015-12-10 02:06:51 -0800

    ROS Workspace initiated

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/test/Linefollow/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/test/Linefollow/catkin_ws/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/test/Linefollow/catkin_ws/devel/lib:/home/test/Linefollow/catkin_ws/devel/lib/x86_64-linux-gnu:/home/test/catkin_ws/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/home/test/catkin_ws/devel/lib:/home/test/PluC/devel/lib:/home/test/ros/catkin_ws/devel/lib:/opt/ros/indigo/lib"
+export PATH="/home/test/Linefollow/catkin_ws/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/test/Linefollow/catkin_ws/devel/lib/pkgconfig:/home/test/Linefollow/catkin_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/home/test/catkin_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/test/catkin_ws/devel/lib/pkgconfig:/home/test/PluC/devel/lib/pkgconfig:/home/test/ros/catkin_ws/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/test/Linefollow/catkin_ws/build"
+export PYTHONPATH="/home/test/Linefollow/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/test/Linefollow/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/test/Linefollow/catkin_ws/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
